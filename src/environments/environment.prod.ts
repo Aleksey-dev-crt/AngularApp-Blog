84f9229bb2d33fc670c26137806c40da38b73764
Aleksey-dev-crt <64360318+Aleksey-dev-crt@users.noreply.github.com>
2020-05-07 14:44:22 +0700

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyAbVKsdSakawJw78J3G2NcifEqv-5cq724',
+  fbDbUrl: 'https://angular-blog-77391.firebaseio.com'
+};
